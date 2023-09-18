6db51f029150d770a6e4a7e9536adeee2df776f7
Browni Wilson <browniwils@gmail.com>
2023-09-18 14:47:26 +0000

    added task 1 for 0x14

---
+++ 0x14-mysql/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Create database user for database server
+
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+
