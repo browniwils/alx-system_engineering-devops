f4f0c1c74817bef39db527b4b944867d35dc9427
Browni Wilson <browniwils@gmail.com>
2023-10-12 15:09:27 +0000

    updated 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -27,8 +27,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     results = results.get("data")
     after = results.get("after")
     count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
+    for words in results.get("children"):
+        title = words.get("data").get("title").lower().split()
         for word in word_list:
             if word.lower() in title:
                 times = len([t for t in title if t == word.lower()])
@@ -42,6 +42,6 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
             print("")
             return
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+        [print("{}: {}".format(k, v)) for k, v in instances.items()]
     else:
         count_words(subreddit, word_list, instances, after, count)
